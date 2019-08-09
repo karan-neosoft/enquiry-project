968333b89e3e48c2695a35b983dbd106446cafa1
13amol <amolborkar30597@gmail.com>
2019-08-09 15:14:57 +0530

    Added comments for code and added django paginator for pagination of data and style the UI of the pagination

--- account/views.py
+++ account/views.py
@@ -2,22 +2,35 @@ from django.shortcuts import render,redirect
 from django.http import JsonResponse
 from .models import Enquiry,AdminLogin
 from .forms import EnquiryForm,AdminForm
+from django.core.paginator import Paginator
 
-
+# all enquiry form data rendered here
 def account(request):
+    """ Render all records of enquiry form  """
+
     if request.session.get('username')=="karan":
         all_records = Enquiry.objects.all()
+        paginator = Paginator(all_records,2)
+        page = request.GET.get('page')
+        all_records = paginator.get_page(page)
         return render(request,'records.html',{"data":all_records})
     return redirect('/enquiry')
 
 
+# edit option for the admin to edit enquiry data which is render
 def edit_data(request,id):
+    """ Show the data for edit in form """
+
     if request.session.get('username')=="karan":
         enquiry_data = Enquiry.objects.get(id=id)  
         return render(request,'admin_enquiry_form.html', {'data':enquiry_data})
     return redirect('/enquiry')
 
+
+#edit data action performed here after submit button is clicked
 def edit_data_updated(request):
+    """ Edit data validation and insert into database """
+
     if request.session.get('username')=="karan":
         form_data = request.POST.dict()
         validity_status = EnquiryForm(form_data)
@@ -32,18 +45,27 @@ def edit_data_updated(request):
     return redirect('/enquiry')
 
 
+#delete  user data
 def delete_data(request,id):
+    """ Delete user data """
+
     if request.session.get('username')=="karan":
         Enquiry.objects.filter(id=id).delete()
         return redirect("/")
     return redirect('/enquiry')
 
 
+#login panel for admin
 def login(request):
+    """ render of the login panel page """
+
     return render(request,'login.html')
 
 
+#login check for admin panel
 def login_action(request):
+    """ Login form validation check """
+    
     formdata = request.POST.dict()
 
     try:
--- enquiry/views.py
+++ enquiry/views.py
@@ -3,15 +3,21 @@ from django.http import JsonResponse
 # Create your views here.
 from .forms import EnquiryForm
 from .models import Enquiry
+
+# enquiry form UI render
 def index(request):
+    """ Enquiry form UI render """
+
     return render(request,'enquiry_form.html')
 
 
+# enquiry form data validation and insert into database
 def enquiry_form_action(request):
+    """ Enquiry form validation and insert operation """
+    
     form_data = request.POST.dict()
     validity_status = EnquiryForm(form_data)
     if not validity_status.is_valid():
-        print(validity_status)
         return JsonResponse(validity_status.errors)
     else:
         Enquiry.objects.create(**form_data)
